8ff5ccfebb0a966ac10ad23de17364398a3fd0a9
agustin omg <agustin.montellano@grupoeho.com>
2022-08-27 15:06:07 -0500

    archivo readme

---
+++ go/plcStat/README.mk
@@ -0,0 +1,13 @@
+ ** plcStat 
+
+La funcionalidad principal de este programa es la obtencion de los parametros de de CPU y memoria del PLC,
+"Se considera que el PLC corre una version de LINUX, las pruebas seran realizadas en un PLC de la marca WAGO , Modelo 750 - 8206"
+
+Una vez obtenido los datos del PLC se generara un servidor HTTP el cual servira los datos de estado del PLC a cualqueir sistema capaz de realizar uan solicitud de tipo GET.
+
+** LOG 
+
+* se genera el packete
+* se selecciona la libreria
+* se prueba la obtencion de de datos de CPU y memoria  -  27/08/2022
+
