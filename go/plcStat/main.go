2fad8969796360eb4a22d94c145ac06162c47b0d
Agustin Ortiz <agustin.montellano@grupoeho.com>
2022-09-01 13:18:47 -0500

    se levanta el servidor HTTP

--- go/plcStat/main.go
+++ go/plcStat/main.go
@@ -3,38 +3,50 @@ package main
 import ( 
   "fmt"
   "os"
-  "time"
+  "strconv"
+//  "time"
+  "io"
+	"net/http"
   "github.com/mackerelio/go-osstat/memory"
-  "github.com/mackerelio/go-osstat/cpu"
+  //"github.com/mackerelio/go-osstat/cpu"
   )
 
-func main() {
+func getMem(w http.ResponseWriter, r *http.Request) {
+	
   memory, err := memory.Get()
-  before, err := cpu.Get()
-
-  // Get the available memory
-   if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		return
-	}
-	fmt.Printf("memory total: %d bytes\n", memory.Total)
-	fmt.Printf("memory used: %d bytes\n", memory.Used)
-	fmt.Printf("memory cached: %d bytes\n", memory.Cached)
-	fmt.Printf("memory free: %d bytes\n", memory.Free)
-
-  // get the CPU status
   if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		return
-	}
-	time.Sleep(time.Duration(1) * time.Second)
-	after, err := cpu.Get()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		return
-	}
-	total := float64(after.Total - before.Total)
-	fmt.Printf("cpu user: %f %%\n", float64(after.User-before.User)/total*100)
-	fmt.Printf("cpu system: %f %%\n", float64(after.System-before.System)/total*100)
-	fmt.Printf("cpu idle: %f %%\n", float64(after.Idle-before.Idle)/total*100)
+ 	fmt.Fprintf(os.Stderr, "%s\n", err)
+ 	return
+ }
+ //fmt.Printf("memory total: %d bytes\n", memory.Total
+fmt.Printf("memory used: %d bytes\n", memory.Used)
+ //fmt.Printf("memory cached: %d bytes\n", memory.Cach
+ //fmt.Printf("memory free: %d bytes\n", memory.Free)
+  s := strconv.FormatUint(memory.Used, 10)
+  io.WriteString(w,s)
 }
+func getCpu(w http.ResponseWriter, r *http.Request) {
+  //before, err := cpu.Get()
+ // if err != nil {
+	//	fmt.Fprintf(os.Stderr, "%s\n", err)
+	//	return
+	//}
+	//time.Sleep(time.Duration(1) * time.Second)
+	//after, err := cpu.Get()
+	//if err != nil {
+	  //fmt.Fprintf(os.Stderr, "%s\n", err)
+	//total := float64(after.Total - before.Total)
+	//fmt.Printf("cpu user: %f %%\n", float64(after.User-before.User)/total*100)
+	//fmt.Printf("cpu system: %f %%\n", float64(after.System-before.System)/total*100)
+	//fmt.Printf("cpu idle: %f %%\n", float64(after.Idle-before.Idle)/total*100)
+	io.WriteString(w, "Hello, HTTP!\n")
+}
+
+func main() {
+	http.HandleFunc("/mem", getMem)
+	http.HandleFunc("/cpu", getCpu)
+	err := http.ListenAndServe(":3333", nil)
+  if err != nil{
+    fmt.Printf("ERROR")
+  }
+  }
